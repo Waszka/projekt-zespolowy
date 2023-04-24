a24281e3448c2aedf39cb871382dd5bcdfee17f3
Waszka <filip.toka@wp.pl>
2023-04-24 20:53:25 +0200

    add multiple arduino and schow temp and noise

--- server.js
+++ server.js
@@ -25,25 +25,105 @@ connection.connect((err) => {
 });
 
 
-
-app.get('/api/temp', (req, res) => {
-    const sql = `SELECT * FROM ims ORDER BY datetime DESC LIMIT 1`;
+app.get('/api/arduino', (req, res) => {
+    const sql = `SELECT * FROM arduino`;
 
     // Wykonanie zapytania do bazy danych
     connection.query(sql, (err, result) => {
         if (err) {
             res.status(500).json({error: err.message});
-        }else {
+        } else {
             res.status(200).json(result);
         }
     });
 
 });
 
+app.get('/api/temp/:arduinoId', (req, res) => {
+    const arduinoId = req.params.arduinoId;
+    const sql = `SELECT * FROM temp WHERE arduino_id = '${arduinoId}' ORDER BY datetime DESC LIMIT 1`;
 
+    // Wykonanie zapytania do bazy danych
+    connection.query(sql, (err, result) => {
+        if (err) {
+            res.status(500).json({error: err.message});
+        } else if (result.length === 0) {
+            res.status(404).json({error: 'Not found'});
+        } else {
+            res.status(200).json(result);
+        }
+    });
 
+});
 
+app.get('/api/noise/:arduinoId', (req, res) => {
+    const arduinoId = req.params.arduinoId;
+    const sql = `SELECT * FROM noise WHERE arduino_id = '${arduinoId}' ORDER BY datetime DESC LIMIT 1`;
 
+    // Wykonanie zapytania do bazy danych
+    connection.query(sql, (err, result) => {
+        if (err) {
+            res.status(500).json({error: err.message});
+        } else if (result.length === 0) {
+            res.status(404).json({error: 'Not found'});
+        } else {
+            res.status(200).json(result);
+        }
+    });
+
+});
+
+app.get('/api/camera/:arduinoId', (req, res) => {
+    const arduinoId = req.params.arduinoId;
+    const sql = `SELECT * FROM camera WHERE arduino_id = '${arduinoId}'`;
+
+    // Wykonanie zapytania do bazy danych
+    connection.query(sql, (err, result) => {
+        if (err) {
+            res.status(500).json({error: err.message});
+        } else if (result.length === 0) {
+            res.status(404).json({error: 'Not found'});
+        } else {
+            res.status(200).json(result);
+        }
+    });
+
+});
+
+app.get('/api/allArduino', (req, res) => {
+    const sql = `SELECT arduino.*, temp.temperature, temp.humidity, noise.noiseLevel
+FROM arduino
+LEFT JOIN (
+  SELECT arduino_id, temperature, humidity
+  FROM temp
+  WHERE datetime = (
+    SELECT MAX(datetime)
+    FROM temp
+    WHERE temp.arduino_id = arduino_id
+  )
+) AS temp ON arduino.arduino_id = temp.arduino_id
+LEFT JOIN (
+  SELECT arduino_id, noiseLevel
+  FROM noise
+  WHERE datetime = (
+    SELECT MAX(datetime)
+    FROM noise
+    WHERE noise.arduino_id = arduino_id
+  )
+) AS noise ON arduino.arduino_id = noise.arduino_id;
+`;
+
+    // Wykonanie zapytania do bazy danych
+    connection.query(sql, (err, result) => {
+        if (err) {
+            res.status(500).json({error: err.message});
+        }else {
+            console.log(result);
+            res.status(200).json(result);
+        }
+    });
+
+});
 
 
 // Obsługa nieznanych adresów URL
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,53 +1,69 @@
 <template>
 
 
-    <nav class="border-gray-200 bg-blue-500 dark:bg-gray-800 dark:border-gray-700">
-        <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-            <a href="#" class="flex items-center">
-                <img src="../assets/factory.png" class="h-8 mr-3" alt="Flowbite Logo" />
-                <span class="self-center text-2xl font-semibold whitespace-nowrap text-white">Projekt zespołowy</span>
-            </a>
-            <button data-collapse-toggle="navbar-solid-bg" type="button" class="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-solid-bg" aria-expanded="false">
-                <span class="sr-only">Open main menu</span>
-                <svg class="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
-            </button>
-            <div class="hidden w-full md:block md:w-auto" id="navbar-solid-bg">
-                <ul class="flex flex-col font-medium mt-4 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-transparent dark:bg-gray-800 md:dark:bg-transparent dark:border-gray-700">
-                    <li>
-                        <a href="#" class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Sterowanie</a>
-                    </li>
-                    <li>
-                        <a href="#" class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Monitorowanie</a>
-                    </li>
-                    <li>
-                        <a href="#" class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Zarządzanie</a>
-                    </li>
-                </ul>
-            </div>
-        </div>
-    </nav>
+  <nav class="border-gray-200 bg-blue-500 dark:bg-gray-800 dark:border-gray-700">
+    <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+      <a href="#" class="flex items-center">
+        <img src="../assets/factory.png" class="h-8 mr-3" alt="Flowbite Logo"/>
+        <span class="self-center text-2xl font-semibold whitespace-nowrap text-white">Projekt zespołowy</span>
+      </a>
+      <button data-collapse-toggle="navbar-solid-bg" type="button"
+              class="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+              aria-controls="navbar-solid-bg" aria-expanded="false">
+        <span class="sr-only">Open main menu</span>
+        <svg class="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20"
+             xmlns="http://www.w3.org/2000/svg">
+          <path fill-rule="evenodd"
+                d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
+                clip-rule="evenodd"></path>
+        </svg>
+      </button>
+      <div class="hidden w-full md:block md:w-auto" id="navbar-solid-bg">
+        <ul class="flex flex-col font-medium mt-4 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-transparent dark:bg-gray-800 md:dark:bg-transparent dark:border-gray-700">
+          <li>
+            <a href="#"
+               class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Sterowanie</a>
+          </li>
+          <li>
+            <a href="#"
+               class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Monitorowanie</a>
+          </li>
+          <li>
+            <a href="#"
+               class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Zarządzanie</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
 
   <!-- component -->
   <div class="w-1/2 mt-12 justify-center container mx-auto">
-    <div class="flex flex-wrap w-full lg:w-auto">
+    <div v-for="arduino in arduinos" :key="arduino.id" class="flex flex-wrap w-full lg:w-auto mt-3">
       <div class="w-full lg:w-1/2 flex rounded-lg bg-auto"
            style="background-image: url('https://images.unsplash.com/photo-1559963110-71b394e7494d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=675&q=80')">
         <div class="rounded-lg py-6 pl-8 pr-32 w-full bg-blue-400 opacity-90 text-white">
           <div class="mb-20">
-            <h2 class="font-bold text-3xl leading-none pb-1">Fabryka</h2>
+            <h2 class="font-bold text-3xl leading-none pb-1">Fabryka {{ arduino.arduino_id }}</h2>
           </div>
-          <div>
-              <strong class="leading-none text-5xl block font-weight-bolder">Temperatura</strong>
-            <strong class="leading-none text-4xl block font-weight-bolder">{{ temp }}°C</strong>
+          <div v-if="arduino.temp === 1">
+            <strong class="leading-none text-3xl block font-weight-bolder">Temperatura</strong>
+            <strong class="leading-none text-2xl block font-weight-bolder">{{ arduino.temperature }}°C</strong>
+            <strong class="leading-none text-3xl block font-weight-bolder">Wilgotność</strong>
+            <strong class="leading-none text-2xl block font-weight-bolder">{{ arduino.humidity }}%</strong>
           </div>
         </div>
       </div>
 
       <div class="w-full lg:w-1/2 flex ml-0">
         <div class="lg:my-3 bg-gray-800 text-white p-8 lg:rounded-r-lg w-full">
+          <div v-if="arduino.noise === 1" class="flex justify-between w-64 mb-4 w-full">
+            <div class="w-auto text-2xl font-bold text-90">Głośność</div>
+            <div class="w-auto text-2xl text-right">{{ arduino.noiseLevel }}dB</div>
+          </div>
           <div class="flex justify-between w-64 mb-4 w-full">
-            <div class="w-auto text-3xl font-bold uppercase text-90">Wilgotność</div>
-            <div class="w-auto text-3xl text-right">{{ hum }}%</div>
+            <div v-if="arduino.status === 1" class="w-auto text-2xl font-bold text-90">Online</div>
+            <div v-if="arduino.status === 0" class="w-auto text-2xl font-bold text-90">Offline</div>
           </div>
         </div>
       </div>
@@ -63,8 +79,7 @@ export default {
   name: 'HelloWorld',
   data() {
     return {
-      temp: "0..",
-      hum: "0..",
+      arduinos: [],
 
     };
   },
@@ -72,14 +87,37 @@ export default {
     initFlowbite()
 
     window.setInterval(async () => {
-      await axios.get('http://localhost:3000/api/temp').then(resp => {
-        this.temp = resp.data[0].temperature
-        this.hum = resp.data[0].humidity
-        console.log(resp.data[0])
+      await axios.get('http://localhost:3000/api/allArduino').then(async resp => {
+        this.arduinos = resp.data
+        console.log(resp.data)
       }).catch(error => {
         console.warn(error)
       })
     }, 1000)
+
+    /*window.setInterval(async () => {
+      await axios.get('http://localhost:3000/api/arduino').then(async resp => {
+        this.arduinos = resp.data
+
+        this.arduinos.forEach(arduino => {
+          if (arduino.temp === 1) {
+            axios.get('http://localhost:3000/api/temp/' + arduino.arduino_id).then(resp => {
+              arduino.tempValue = resp.data[0].temperature
+              arduino.humValue = resp.data[0].humidity
+            })
+          }
+
+          if (arduino.noiseLevel === 1) {
+            axios.get('http://localhost:3000/api/noise/' + arduino.arduino_id).then(resp => {
+              arduino.noiseValue = resp.data[0].noiseLevel
+            })
+          }
+        })
+        console.log(this.arduinos)
+      }).catch(error => {
+        console.warn(error)
+      })
+    }, 1000)*/
 
   }
 }
